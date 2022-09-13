f8b7040e97284c977a111771bfad056e8af8ee57
Volodymyr Bodnarchuk <volodymyr.bodnarchuk@blockchainvoucher.com>
2022-09-13 18:09:24 +0300

    feat: attempt to host

--- server/server.js
+++ server/server.js
@@ -5,32 +5,32 @@ const io = require('socket.io');
 const cors = require('cors');
 
 const INTERVAL = 1000;
-const PORT = 3002;
+// const PORT = 3002;
 
 const horses = [
   {
     name: 'Princess Diana',
-    distance: 0
+    distance: 0,
   },
   {
     name: 'Cricket',
-    distance: 0
+    distance: 0,
   },
   {
     name: 'Rebel',
-    distance: 0
+    distance: 0,
   },
   {
     name: 'Lucy',
-    distance: 0
+    distance: 0,
   },
   {
     name: 'Lacey',
-    distance: 0
+    distance: 0,
   },
   {
     name: 'Ginger',
-    distance: 0
+    distance: 0,
   },
 ];
 
@@ -42,15 +42,13 @@ function randomValue() {
 }
 
 function getRound(socket) {
-
-  const round = horses.map(horse => {
-    const currentDistance = horse.distance += randomValue();
+  const round = horses.map((horse) => {
+    const currentDistance = (horse.distance += randomValue());
 
     return {
       name: horse.name,
-      distance: maxDistance < currentDistance ? maxDistance : currentDistance
-    }
-    
+      distance: maxDistance < currentDistance ? maxDistance : currentDistance,
+    };
   });
 
   socket.emit('ticker', round);
@@ -59,13 +57,13 @@ function getRound(socket) {
 function trackTickers(socket) {
   getRound(socket);
 
-  const timer = setInterval(function() {
+  const timer = setInterval(function () {
     getRound(socket);
   }, INTERVAL);
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     clearInterval(timer);
-    horses.map(horse => horse.distance = 0);
+    horses.map((horse) => (horse.distance = 0));
   });
 }
 
@@ -75,21 +73,23 @@ const server = http.createServer(app);
 
 const socketServer = io(server, {
   cors: {
-    origin: "*",
-  }
+    origin: '*',
+  },
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 socketServer.on('connection', (socket) => {
   socket.on('start', () => {
-    horses.map(horse => horse.distance = 0);
+    horses.map((horse) => (horse.distance = 0));
     trackTickers(socket);
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Streaming service is running on http://localhost:${PORT}`);
+server.listen('https://racing-horses-server.herokuapp.com', () => {
+  console.log(
+    `Streaming service is running on https://racing-horses-server.herokuapp.com`
+  );
 });
